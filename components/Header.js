a2949186b193b47290c3e468ac677d9edf81d260
jeekdong <jeekdong@gmail.com>
2022-04-13 11:32:41 +0800

    feat: 更新

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,9 @@
 import { useEffect, useRef } from 'react'
+import Image from 'next/image'
 import Link from 'next/link'
 import BLOG from '@/blog.config'
 import { useLocale } from '@/lib/locale'
+import { EMAIL_HASH } from '@/lib/constants'
 
 const NavBar = () => {
   const locale = useLocale()
@@ -68,41 +70,26 @@ const Header = ({ navBarTitle, fullWidth }) => {
       >
         <div className="flex items-center">
           <Link href="/">
-            <a aria-label={BLOG.title}>
-              <div className="h-6">
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <rect
-                    width="24"
-                    height="24"
-                    className="fill-current text-black dark:text-white"
-                  />
-                  <rect width="24" height="24" fill="url(#paint0_radial)" />
-                  <defs>
-                    <radialGradient
-                      id="paint0_radial"
-                      cx="0"
-                      cy="0"
-                      r="1"
-                      gradientUnits="userSpaceOnUse"
-                      gradientTransform="rotate(45) scale(39.598)"
-                    >
-                      <stop stopColor="#CFCFCF" stopOpacity="0.6" />
-                      <stop offset="1" stopColor="#E9E9E9" stopOpacity="0" />
-                    </radialGradient>
-                  </defs>
-                </svg>
-              </div>
+            <a aria-label={BLOG.title} className="flex items-center">
+              <Image
+                alt={BLOG.author}
+                width={24}
+                height={24}
+                src={`https://cravatar.cn/avatar/${EMAIL_HASH}`}
+                className="rounded-full mb-2"
+              />
+              {
+                !navBarTitle && (
+                  <span className="inline-block ml-2">
+                    Jeekdong
+                  </span>
+                )
+              }
             </a>
           </Link>
           {navBarTitle
             ? (
-            <p className="ml-2 font-medium text-day dark:text-night header-name">
+            <p className="ml-8 font-medium text-day dark:text-night header-name">
               {navBarTitle}
             </p>
               )
